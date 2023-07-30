ff5ed4cac333d64fca00641bdf9f83a8df61e4f3
irfan <iamqirfan@gmail.com>
2023-07-30 23:39:52 +0530

    getting obj from 1 component to other

--- my-app/src/app/components/user-details/user-details.component.ts
+++ my-app/src/app/components/user-details/user-details.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GithubUser } from 'src/app/types';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent {
+  @Input()
+  detail:GithubUser[] = [];
 
 }
